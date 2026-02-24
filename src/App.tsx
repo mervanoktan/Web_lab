88c59644ec10d6e57bb35c9063a5c71fd1f7a00a
mervanoktan <230541144@firat.edu.tr>
2026-02-24 22:07:44 +0300

    chore: initial React + TypeScript project setup (LAB-1)

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import'./App.css'
+
+
+function App() {
+
+return (
+<div>
+<h1>Web Tasarimi ve Programlama</h1>
+<h2>LAB-1</h2>
+<p>Ad Soyad: [Mervan OKTAN]</p>
+<p>Ogrenci No: [230541144]</p>
+</div>
+)
+ }
+export default App
